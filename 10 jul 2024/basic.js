1e74c0f781a0623d09de432241daeeb3ef99bd3c
sowmya-mora20 <sowmyamora20@gmail.com>
2024-07-13 17:40:27 +0530

    first commit

---
+++ 10 jul 2024/basic.js
@@ -0,0 +1,8 @@
+console.log("hello world");// print like statements will be visible in console(insepct>>console)
+//3 methods to define variables-> var(not used nawadays, older way), 
+//let(more used these days, modern way), 
+//const(used to declare variable, but the ines, whose value are not changeable)
+var a=1;
+const b=5;
+let c=a+b;
+console.log(c);
